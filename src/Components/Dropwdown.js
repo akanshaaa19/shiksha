3ba0f6729169a653461fcf7f5b55f3dae8f428b3
sanidhya004 <80572505+sanidhya004@users.noreply.github.com>
2023-02-14 17:38:22 +0530

    akansha dekh lena

--- src/Components/Dropwdown.js
+++ src/Components/Dropwdown.js
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const Dropwdown = () => {
   return (
+  <>
+  <div className='h-[30vh] flex justify-center flex-col items-center '>
+       <p className='text-4xl capitalize font-bold'>Data Science</p>
+       <p className='text-xs text-center px-7'>Data science is a "concept to unify statistics, data analysis, informatics, and their related methods" in order to "understand and analyse actual phenomena" with data.[5] It uses techniques and theories drawn from many fields within the context of mathematics, statistics, computer science, information science, and domain knowledge.[6] However, data science is different from computer science and information science. Turing Award winner Jim Gray imagined data science as a "fourth paradigm" of science (empirical, theoretical, computational, and now data-driven) and asserted that "everything about science is changing because of the impact of information technology" and the data deluge.[7][8]</p>
+  </div>
     <section className="max-w-5xl mx-auto py-10">
   <div>
     <div className="flex flex-row">
@@ -86,6 +91,7 @@ export const Dropwdown = () => {
         </div>
       </div>
     </div>
+    {/* replicate this */}
     <div className="flex items-start flex-row-reverse">
       <div className="border-t-4 border-l-4 border-transparent">
         <div className="w-16 mr-16 h-16 border-r-4 border-gray-300 border-dashed border-b-4 rounded-br-full" />
@@ -131,6 +137,7 @@ export const Dropwdown = () => {
         </div>
       </div>
     </div>
+    {/* end here */}
     <div className="flex items-start flex-row">
       <div className="border-t-4 border-r-4 border-transparent">
         <div className="w-16 ml-16 h-16 border-l-4 border-gray-300 border-dashed border-b-4 rounded-bl-full" />
@@ -172,9 +179,60 @@ export const Dropwdown = () => {
           </div>
         </div>
       </div>
+      
+      
     </div>
+    <div className="flex items-start flex-row-reverse">
+      <div className="border-t-4 border-l-4 border-transparent">
+        <div className="w-16 mr-16 h-16 border-r-4 border-gray-300 border-dashed border-b-4 rounded-br-full" />
+      </div>
+      <div className="border-t-4 border-transparent flex-auto">
+        <div className="h-16 border-b-4 border-gray-300 border-dashed" />
+      </div>
+      <div className="w-16 mt-16 ml-16 h-16 border-l-4 border-gray-300 border-dashed border-t-4 rounded-tl-full" />
+    </div>
+    <div className="flex flex-row">
+      <div className="hidden md:flex flex-col items-center">
+        <div className="w-32 py-5 border border-gray-300 rounded mr-4 uppercase flex flex-col items-center justify-center">
+          <div className="text-3xl font-black text-gray-500">Step 3</div>
+          <div className="text-gray-500 text-sm">Planification</div>
+        </div>
+        <div className="h-full border-l-4 border-transparent">
+          <div className="border-l-4 mr-4 h-full border-gray-300 border-dashed" />
+        </div>
+      </div>
+      <div className="flex-auto border rounded  border-gray-300">
+        <div className="flex md:flex-row flex-col items-center">
+          <div className="flex-auto">
+            <div className="md:hidden text-sm font-normal uppercase pt-3 pl-3 text-gray-500">
+              <span className="font-black">Step 3</span> - Planification
+            </div>
+            <div className="p-3 text-3xl text-gray-800 font">
+              Make a good plan and prepare tasks
+            </div>
+            <div className="px-3 pb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+              facilis, voluptates error alias dolorem praesentium sit soluta
+              iure incidunt labore explicabo eaque, quia architecto veritatis
+              dolores, enim consequatur nihil ipsum.
+            </div>
+          </div>
+          <div className="md:w-96 w-full p-5">
+            <img
+              src="https://image.flaticon.com/icons/svg/1330/1330216.svg"
+              alt="step 3"
+              className="object-scale-down"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
   </div>
+  
 </section>
+</>
 
 
   )
